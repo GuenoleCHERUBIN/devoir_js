0aa5a4515ce9a64e8681db068735a637f2dc1f96
SKLadeR <guenole.cherubin@gmail.com>
2019-07-12 12:56:43 +0200

    update readme and comments

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
+//app.js
 
-const app = express();
+const http = require('http'); //allow us to use http and it's methods
+const express = require('express'); //allow us to use express and it's methods
+const bodyParser = require('body-parser'); //allow us to use body-parser and it's methods
+
+const app = express();//initialize express
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -14,6 +16,7 @@ const server = http.createServer((req, res) => {
        
     `);
 });
+//initialize the variables to allow us to access the serv
 const hostname = '127.0.0.1';
 const port = 3600;
 
